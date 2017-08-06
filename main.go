b834676ba1a737da20e8bca96ce7edf73f49346d
Esther Sanchez <sanchez.ther@gmail.com>
2017-08-06 21:41:35 +0200

    http get url and print body if status is 200

--- main.go
+++ main.go
@@ -1,12 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
 
 func main() {
 	user := "thersanchez"
 	repo := "caesar"
 	url := issueURL(user, repo)
-	fmt.Println(url)
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		log.Fatal("expected status 200, got %d", resp.StatusCode)
+	}
+	printReader(resp.Body)
 }
 
 // issueURL returns the unquoted URL to access the issues from the Github
@@ -14,4 +27,20 @@ func main() {
 func issueURL(user, repo string) string {
 	const urlFmt = "https://api.github.com/repos/%s/%s/issues"
 	return fmt.Sprintf(urlFmt, user, repo)
+}
+
+// printReader prints the bytes read from r to stdout in chunks of 1024 bytes
+func printReader(r io.Reader) {
+	chunk := make([]byte, 1024)
+	var err error
+	var n int
+	for err != io.EOF {
+		n, err = r.Read(chunk)
+		if n > 0 {
+			fmt.Print(string(chunk))
+		}
+		if err != nil && err != io.EOF {
+			log.Fatalf("reading chunk: %s", err)
+		}
+	}
 }
